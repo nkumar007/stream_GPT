02c298fbaeb9d2d3e7d845254b9b97baa61c01e3
nkumar007 <naveen.mbuff98@gmail.com>
2023-08-28 15:13:43 +0530

    Refactor: loginform

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import logo from "../assets/netflix_logo.png";
 export const Header = () => {
   return (
-    <div className="absolute py-2 px-6 bg-gradient-to-b from-black z-10">
-      <img className="w-44" src={logo} alt="logo" />
+    <div className="absolute py-2 px-6  z-10">
+      <img className="w-52" src={logo} alt="logo" />
     </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,9 +3,9 @@ import { Header } from "./Header";
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   return (
-    <div>
+    <div className="relative h-screen w-screen overflow-hidden">
       <Header />
-      <div className="absolute">
+      <div className="absolute  object-cover transform scale-105 brightness-75">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           alt="background_image"
