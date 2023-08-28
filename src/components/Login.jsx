70970fa5e2b6101f8d12b7527607c56beb26ebbf
nkumar007 <naveen.mbuff98@gmail.com>
2023-08-28 14:43:22 +0530

    build signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { Header } from "./Header";
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
   return (
     <div>
       <Header />
@@ -10,8 +12,17 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 absolute p-12 bg-[rgba(0,0,0,.75)] text-white my-36 mx-auto right-0 left-0 ">
-        <h2 className="text-3xl mb-6 font-bold">Sign In</h2>
+        <h2 className="text-3xl mb-6 font-bold">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h2>
         <div className="flex flex-col gap-4 opacity-100">
+          {!isSignInForm && (
+            <input
+              type="text"
+              placeholder="Name"
+              className="p-3 bg-[#333] outline outline-0 rounded-md"
+            />
+          )}
           <input
             type="text"
             placeholder="Email or phone number"
@@ -25,23 +36,29 @@ const Login = () => {
           <input
             type="submit"
             className=" bg-red-600 p-3 cursor-pointer mt-6 rounded-md"
-            value="Sign In"
+            value={isSignInForm ? "Sign In" : "Sign Up"}
           />
-          <div className="flex justify-between items-center text-gray-400">
-            <label>
-              <input type="checkbox" />
-              <span> Remember me</span>
-            </label>
-            <span>Need help?</span>
-          </div>
+          {isSignInForm && (
+            <div className="flex justify-between items-center text-gray-400">
+              <label>
+                <input type="checkbox" />
+                <span> Remember me</span>
+              </label>
+              <span>Need help?</span>
+            </div>
+          )}
+
           <div className="text-gray-400 text-sm gap-4 flex flex-col">
             <div>
-              New to StreamGPT?{" "}
-              <span className="text-white hover:underline cursor-pointer">
-                Sign up now
+              {isSignInForm ? "New to Netflix?" : "Already a user?"}{" "}
+              <span
+                className="text-white hover:underline cursor-pointer"
+                onClick={() => setIsSignInForm(!isSignInForm)}
+              >
+                {isSignInForm ? "Sign up now" : "Sign In"}
               </span>
             </div>
-            <div>
+            <div className={!isSignInForm ? "hidden" : "block"}>
               This page is protected by Google reCAPTCHA to ensure you are not a
               bot{" "}
               <span className="text-[#0071eb] cursor-pointer hover:underline">
